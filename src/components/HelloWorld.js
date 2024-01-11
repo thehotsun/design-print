4e873a3fedabd4878aa824c86dbd76e1291feade
张学强 <zhang.xueqiang>
2024-01-11 11:16:32 +0800

    feat: 添加项目配置

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,121 @@
+import getRenderOptions from '@/define/renderOptions'
+import './index.less'
+export default {
+  name: 'designTable',
+  props: {
+    onlyShow: Boolean,
+    formData: Object,
+  },
+  data() {
+    return { formRef: 'q', renderOptions: {} }
+  },
+
+  watch: {},
+  async created() {
+    this.init()
+  },
+
+  methods: {
+    init() {
+      this.renderOptions = getRenderOptions()
+    },
+    getTdComp(tdOptions) {
+      const { attrs = {}, options = {} } = tdOptions
+
+      const listeners = {}
+      return (
+        <td
+          {...{
+            attrs,
+            on: listeners,
+          }}
+        >
+          <div>{options.value}</div>
+        </td>
+      )
+    },
+    getTrComp(options) {
+      const { getTdComp } = this
+      const { attrs = {}, tdList = [] } = options
+      const listeners = {}
+      return (
+        <tr
+          {...{
+            attrs,
+            on: listeners,
+          }}
+        >
+          {tdList.map((item) => {
+            return getTdComp(item)
+          })}
+        </tr>
+      )
+    },
+    getTbodyComp(bodyOptions) {
+      const { getTrComp } = this
+      const { attrs = {}, trList = [] } = bodyOptions
+      const listeners = {}
+      return (
+        <tbody
+          {...{
+            attrs,
+            on: listeners,
+          }}
+        >
+          {trList.map((trOptions) => {
+            return getTrComp(trOptions)
+          })}
+        </tbody>
+      )
+    },
+    getTheadComp(headOptions) {
+      const { getTrComp } = this
+      const { attrs = {}, trOptions = {} } = headOptions
+      const listeners = {}
+      if (Object.keys(trOptions).length) {
+        return (
+          <thead
+            {...{
+              attrs,
+              on: listeners,
+            }}
+          >
+            {getTrComp(trOptions)}
+          </thead>
+        )
+      } else {
+        return
+      }
+    },
+    getTableComp(tableOptions) {
+      const {
+        headOptions = {},
+        bodyOptions = {},
+        attrs = {},
+        useThead = false,
+      } = tableOptions
+      const listeners = {}
+      const { getTbodyComp, getTheadComp } = this
+      return (
+        <table
+          {...{
+            attrs,
+            on: listeners,
+          }}
+        >
+          {useThead ? getTheadComp(headOptions) : ''}
+          {getTbodyComp(bodyOptions)}
+        </table>
+      )
+    },
+  },
+
+  render() {
+    const { formRef, getTableComp, renderOptions } = this
+    return (
+      <div class="designTableWrapper" ref={formRef}>
+        {getTableComp(renderOptions)}
+      </div>
+    )
+  },
+}
---
+++ src/define/renderOptions.js
@@ -0,0 +1,111 @@
+function getRenderOptions() {
+  return {
+    headOptions: {
+      attrs: {},
+      trOptions: {
+        attrs: {},
+        tdList: [],
+      },
+    },
+    bodyOptions: {
+      attrs: {},
+      trList: [
+        {
+          attrs: {},
+          tdList: [
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 2,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+          ],
+        },
+        {
+          attrs: {},
+          tdList: [
+            {
+              attrs: {
+                colspan: 3,
+                rowspan: 2,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+          ],
+        },
+        {
+          attrs: {},
+          tdList: [
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+          ],
+        },
+        {
+          attrs: {},
+          tdList: [
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+            {
+              attrs: {
+                colspan: 1,
+                rowspan: 1,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    attrs: {},
+    useThead: false,
+  }
+}
+export default getRenderOptions
